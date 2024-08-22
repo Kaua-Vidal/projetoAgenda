3e705a7076ff087b5f4531269851ef552b34aa0b
Kaua-Vidal <kaua.vidal@dcx.ufpb.br>
2024-08-22 11:38:09 -0300

    adicionando gravador e leitor de dados, juntamente com a janela GUI

---
+++ src/main/java/dcx/ufpb/br/kaua/agenda/AgendaGUI.java
@@ -0,0 +1,15 @@
+package dcx.ufpb.br.kaua.agenda;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class AgendaGUI extends JFrame {
+
+    public AgendaGUI(){
+        setTitle("Esse é o titulo");
+        setSize(400, 200);
+        setLocation(150, 150);
+        setResizable(false);
+        getContentPane().setBackground(Color.GRAY);
+    }
+}
